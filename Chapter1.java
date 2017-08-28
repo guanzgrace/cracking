fff69c12614b7f6bc3b33a260964f51737190d2f
guanzgrace <16graceg@gmail.com>
2017-08-27 17:16:08 -0700

    8/27/17

--- Chapter1.java
+++ Chapter1.java
@@ -15,11 +15,7 @@ import java.util.*;
 import java.io.*;
 public class Chapter1 {
   
-  /**
-   * Question 1. Correct but uses extra space. More accurate solution would be to use
-   * key-index counting. If we aren't allowed to use extra space, sort the array and then
-   * compare each term to previous terms.
-   */ 
+  // 1.1
   public boolean isUnique(String input) {
     Hashtable<Character, Integer> dups = new Hashtable<Character, Integer>();
     for (int i = 0; i < input.length(); i++) {
@@ -33,7 +29,48 @@ public class Chapter1 {
     }
     return true;
   }
+
+  // 1.1 no extra space
+  public boolean isUniqueNoExtraSpace(String input) {
+    char[] chars = input.toCharArray();
+    Arrays.sort(chars);
+    for (int i = 1; i < chars.length; i++) {
+      if (chars[i - 1] == chars[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
   
-  
+  // 1.2
+  public boolean checkPermutations(String input1, String input2) {
+    Hashtable<Character, Integer> firstString = new HashTable<Character, Integer>();
+    Hashtable<Character, Integer> secondString = new HashTable<Character, Integer>();
+    for (int i = 0; i < input1.length(); i++) {
+      char current = input1.charAt(i);
+      if (firstString.containsKey(current)) {
+        firstString.put(current, firstString.get(current) + 1);
+      } else {
+        firstString.put(current, 1);
+      }
+    }
+
+    for (int i = 0; i < input2.length(); i++) {
+      char current = input2.charAt(i);
+      if (secondString.containsKey(current)) {
+        secondString.put(current, secondString.get(current) + 1);
+      } else {
+        secondString.put(current, 1);
+      }
+    }
+
+    Set<Character> keys = firstString.keySet();
+    for (Character c : keys) {
+      if (firstString.get(c) != secondString.get(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
   
 }
