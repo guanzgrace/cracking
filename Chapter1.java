889040212534091a3ce7cb8c42e3e41d80103c20
guanzgrace <16graceg@gmail.com>
2017-08-27 20:25:09 -0700

    8/27/17

--- Chapter1.java
+++ Chapter1.java
@@ -177,7 +177,50 @@ public class Chapter1 {
   }
 
   // 1.8
-  public void zeroMatrix(int[][] matrix) {
-    
+  public int[][] zeroMatrix(int[][] matrix) {
+    boolean firstrow = false;
+    boolean firstcol = false;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+          if (matrix[i][j] == 0) {
+            if (i == 0) firstrow = true;
+            if (j == 0) firstcol = true;
+            matrix[i][0] = 0;
+            matrix[0][j] == 0;
+          }
+      }
+    }
+
+    for(int i = 1; i < matrix.length; i++) {
+        for(int j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if(firstrow) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if(firstcol) {
+        for(int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    return matrix;
   } 
+
+  // helper method for 1.9
+  private boolean isSubstring(String a, String b) {
+    return true;
+  }
+
+  // 1.9
+  public boolean stringRotation(String s1, String s2) {
+    if (s1.length() != s2.length()) {
+      return false;
+    }
+    return isSubstring(s2, s1 + s1);
+  }
 }
