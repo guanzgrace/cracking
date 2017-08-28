b9410f3541b6718605072c2ddae64432163d5ccb
guanzgrace <16graceg@gmail.com>
2017-08-27 18:36:15 -0700

    8/27/17

--- Chapter1.java
+++ Chapter1.java
@@ -73,4 +73,111 @@ public class Chapter1 {
     return true;
   }
   
+  // 1.3
+  public char[] urlify(String input1, int length) {
+    int spaces = 0;
+    for (int i = 0; i < length; i++) {
+      char current = input1.charAt(i);
+      if (current == ' ') {
+        spaces++;
+      }
+    }
+
+    char[] url = new char[length + 2 * spaces];
+    urlIndex = 0;
+    for (int i = 0; i < length; i++) {
+      char current = input1.charAt(i);
+      if (current == ' ') {
+        url[urlIndex] = '%'; 
+        url[urlIndex + 1] = '2'; 
+        url[urlIndex + 2] = '0';
+        urlIndex = urlIndex + 3; 
+      } else {
+        url[urlIndex] = current;
+        urlIndex++;
+      }
+    }
+    return url;
+  }
+
+  // 1.4
+  public boolean palindromePermutation(String s) {
+    // to be a palindrome permutation, all or all but one letters in s need to 
+    // occur an even number of times
+    char[] input = s.replaceAll(" ", "").toCharArray();
+    Hashtable<String, Integer> t = new Hashtable<String, Integer>();
+    for (int i = 0; i < input.length; i++) {
+      char current = input[i];
+      if (t.containsKey(current)) {
+        t.put(current, t.get(current) + 1);
+      } else {
+        t.put(current, 1);
+      }
+    }
+
+    Set<Character> keys = t.keySet();
+    int odd = 0;
+    for (Character c : keys) {
+      if (firstString.get(c) % 2 == 1) {
+        odd++;
+      }
+    }
+    return odd == 0 || odd == 1;
+  }
+
+  // 1.5 copied from leetcode 
+  public boolean isOneEditDistance(String s, String t) {
+    for (int i = 0; i < Math.min(s.length(), t.length()); i++) { 
+      if (s.charAt(i) != t.charAt(i)) {
+        if (s.length() == t.length()) // s has the same length as t, so the only possibility is replacing one char in s and t
+          return s.substring(i + 1).equals(t.substring(i + 1));
+      else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
+        return s.substring(i).equals(t.substring(i + 1));           
+      else // s is longer than t, so the only possibility is deleting one char from s
+        return t.substring(i).equals(s.substring(i + 1));
+      }
+    }       
+    //All previous chars are the same, the only possibility is deleting the end char in the longer one of s and t 
+    return Math.abs(s.length() - t.length()) == 1;        
+  }
+
+  // 1.6
+  public String stringCompression(String s) {
+    StringBuilder b = new StringBuilder();
+    char prev = s.charAt(0);
+    int count = 1;
+    for (int i = 1; i < s.length; i++) {
+      char current = s.charAt(i);
+      if (prev != current) {
+        b.append(prev);
+        b.append(count);
+        count = 1;
+      } else {
+        count++;
+      }
+    }
+    if (b.length() >= s.length()) {
+      return s;
+    }
+    return b.toString();
+  }
+
+  // 1.7
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < n / 2; i++) {
+      for (int j = 0; j < Math.ceil(((double) n) / 2.); j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[n-1-j][i];
+        matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+        matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+        matrix[j][n-1-i] = temp;
+      }
+    }
+  }
+
+  // 1.8
+  public void zeroMatrix(int[][] matrix) {
+    
+  } 
 }
