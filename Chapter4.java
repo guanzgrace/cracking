3864d013bf74d5a1b8026d63d29f5a3c70d35b9a
guanzgrace <16graceg@gmail.com>
2017-09-04 21:26:58 -0700

    9/4/17

--- Chapter4.java
+++ Chapter4.java
@@ -98,6 +98,27 @@ public class Chapter4 {
 		return result;
 	}
 
+	// 4.4
+	public int checkHeight(TreeNode root) {
+		if (root == null) return -1;
+		int leftHeight = checkHeight(root.left);
+		if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int rightHeight = checkHeight(root.right);
+		if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int heightDiff = leftHeight - rightHeight;
+		if (Math.abs(heightDiff) > 1) {
+			return Integer.MIN_VALUE; // Found error -> pass it back
+		} else {
+			return Math.max(leftHeight, rightHeight) + 1;
+		}
+	}
+
+	public boolean isBalanced(TreeNode root) {
+		return checkHeight(root) != Integer.MIN_VALUE;
+	}
+
 	// interview game
 	 public void game(int[] input){
         int n = input[0];
